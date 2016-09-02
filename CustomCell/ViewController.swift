f253f7df3a71bd49b378f9d62eebfcba00f32c17
Juan Morillo <juanmorillios@gmail.com>
2016-09-02 15:01:59 +0200

    Update Images and Labels
    Update Images and Labels

--- CustomCell/ViewController.swift
+++ CustomCell/ViewController.swift
@@ -22,21 +22,68 @@ class TableViewController: UITableViewController {
     
     override func viewDidLoad() {
         
-        arrayOfCellData = [cellData(cell : 1, text : "", image : ), ]
+        arrayOfCellData = [cellData(cell : 1, text : "Barcelona 1", image : #imageLiteral(resourceName: "barcelos-705973_640") ),
+                           cellData(cell : 2, text : "Barcelona 2", image : #imageLiteral(resourceName: "barcelos-705968_640") ),
+                        cellData(cell : 1, text : "Garden", image : #imageLiteral(resourceName: "gardens-458292_640") )]
     }
     
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return 2
+      return arrayOfCellData.count
+    
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        if arrayOfCellData[indexPath.row].cell == 1 {
+        
+            let cell = Bundle.main.loadNibNamed("TableViewCell1", owner: self, options: nil)?.first as! TableViewCell1
+            
+            cell.mainImageView.image = arrayOfCellData[indexPath.row].image
+            cell.mainLabel.text = arrayOfCellData[indexPath.row].text
+            
+            return cell
+        
+        
+        } else  if arrayOfCellData[indexPath.row].cell == 2 {
+            
+            let cell = Bundle.main.loadNibNamed("TableViewCell2", owner: self, options: nil)?.first as! TableViewCell2
+            
+            cell.mainImageView.image = arrayOfCellData[indexPath.row].image
+            cell.mainLabel.text = arrayOfCellData[indexPath.row].text
+            
+            return cell
+            
+            
+        } else {
+        
+            let cell = Bundle.main.loadNibNamed("TableViewCell1", owner: self, options: nil)?.first as! TableViewCell1
+            
+            cell.mainImageView.image = arrayOfCellData[indexPath.row].image
+            cell.mainLabel.text = arrayOfCellData[indexPath.row].text
+            
+            return cell
+        
+        
+        }
+
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        <#code#>
+        if arrayOfCellData[indexPath.row].cell == 1 {
+        
+            return 250
+ 
+        }
+        else if arrayOfCellData[indexPath.row].cell == 2 {
+         
+           return 70
+        }
+        else {
+        
+            return 250
+        }
+    
     }
    
     
